780e40f61aeec7fedeae09813870e95bd24f2482
Mnkt <Mnkt@DESKTOP-63SAHBN>
2016-06-03 23:03:00 +0200

    Test GH 2

--- MindgemAPI/Controllers/MindgemController.cs
+++ MindgemAPI/Controllers/MindgemController.cs
@@ -28,7 +28,7 @@ namespace MindgemAPI.Controllers
         //Pour acceder à ça : http://localhostIX.../api/mindgem/
         public String getEthereumPrice()
         {
-            // Test GH 1
+            // Test GH 2
             return Convert.ToString(this.kModel.getCurrentEtherPrice("ETH", "EUR"));
         }
     }
