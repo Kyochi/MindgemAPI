c008a96eb0cd55548480225ddaa9fa5f37f168f2
Kyochi <baptistech@hotmail.fr>
2016-06-03 22:38:44 +0200

    Ajout du contrôleur personnalisé dérivant de ApiController

--- MindgemAPI/Controllers/MindgemController.cs
+++ MindgemAPI/Controllers/MindgemController.cs
@@ -1,24 +1,18 @@
-﻿using System.Linq;
-using System.Threading.Tasks;
-using System.Web.Http;
-using System.Web.Http.Controllers;
-using System.Web.Http.OData;
-using Microsoft.WindowsAzure.Mobile.Service;
-using MindgemAPI.DataObjects;
+﻿using Microsoft.WindowsAzure.Mobile.Service;
 using MindgemAPI.Models;
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
 
 namespace MindgemAPI.Controllers
 {
-    public class MindgemController : TableController<WalletItem>
+    public class MindgemController : ApiController
     {
-        protected override void Initialize(HttpControllerContext controllerContext)
-        {
-            base.Initialize(controllerContext);
-            MobileServiceContext context = new MobileServiceContext();
-            DomainManager = new EntityDomainManager<WalletItem>(context, Request, Services);
-        }
-
+        public ApiServices Services { get; set; }
+        
         public const uint MAXUSERS = 10;
 
         KrakenModel[] dataAccount;
@@ -31,43 +25,10 @@ namespace MindgemAPI.Controllers
             dataAccount = new KrakenModel[MAXUSERS];
         }
 
-        //Pour acceder à ça : http://localhost:3213/tables/mindgem/getethereumprice/
+        //Pour acceder à ça : http://localhostIX.../api/mindgem/
         public String getEthereumPrice()
         {
             return Convert.ToString(this.kModel.getCurrentEtherPrice("ETH", "EUR"));
         }
-
-        /*
-        // GET tables/TodoItem
-        public IQueryable<TodoItem> GetAllTodoItems()
-        {
-            return Query();
-        }
-
-        // GET tables/TodoItem/48D68C86-6EA6-4C25-AA33-223FC9A27959
-        public SingleResult<TodoItem> GetTodoItem(string id)
-        {
-            return Lookup(id);
-        }
-
-        // PATCH tables/TodoItem/48D68C86-6EA6-4C25-AA33-223FC9A27959
-        public Task<TodoItem> PatchTodoItem(string id, Delta<TodoItem> patch)
-        {
-            return UpdateAsync(id, patch);
-        }
-
-        // POST tables/TodoItem
-        public async Task<IHttpActionResult> PostTodoItem(TodoItem item)
-        {
-            TodoItem current = await InsertAsync(item);
-            return CreatedAtRoute("Tables", new { id = current.Id }, current);
-        }
-
-        // DELETE tables/TodoItem/48D68C86-6EA6-4C25-AA33-223FC9A27959
-        public Task DeleteTodoItem(string id)
-        {
-            return DeleteAsync(id);
-        }
-        */
     }
 }
