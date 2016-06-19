a949f1cf92aefe2b6d54723795f4bb597e60ea75
Kazuia <Kazuia@users.noreply.github.com>
2016-06-20 00:21:22 +0200

    Initialisation poloniex public data

---
+++ MindgemAPI/Models/poloniex/PoloniexPublicMarketModel.cs
@@ -0,0 +1,24 @@
+﻿using MindgemAPI.utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MindgemAPI.Models.poloniex
+{
+    public class PoloniexPublicMarketModel
+    {
+        private const String URL_PUBLIC_TICKER_POLONIEX = "https://poloniex.com/public?command=returnTicker";
+        private const String URL_PUBLIC_SERVERTIME_POLONIEX = "";
+        private const String URL_PUBLIC_ORDERBOOK_POLONIEX = "";
+
+        public UrlBuilder urlBuilder;
+        public DataObjectProvider dataObjectProvider;
+
+        public PoloniexPublicMarketModel()
+        {
+            urlBuilder = new UrlBuilder();
+            dataObjectProvider = new DataObjectProvider();
+        }
+    }
+}
