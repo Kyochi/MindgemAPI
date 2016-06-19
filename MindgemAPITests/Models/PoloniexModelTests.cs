74980448c1f0a78599089a0a2e34afc30b1b0c03
Kazuia <Kazuia@users.noreply.github.com>
2016-06-20 00:44:45 +0200

    Initialisation premier test getJson pour Poloniex

---
+++ MindgemAPITests/Models/PoloniexModelTests.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MindgemAPI.Models.poloniex;
+using MindgemAPI.utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MindgemAPITests.Models
+{
+    [TestClass]
+    public class PoloniexModelTests
+    {
+        private PoloniexPublicMarketModel pTest = new PoloniexPublicMarketModel();
+        DataObjectProvider dopTest = new DataObjectProvider();
+        private const string ethereum = "ETH";
+        private const string bitcoin = "BTC";
+        private const string euro = "EUR";
+        private const string dollar = "USD";
+        private const string fake = "fakeCurrency";
+
+        [TestMethod]
+        public void getPoloniexJsonTest()
+        {
+            Assert.AreEqual(pTest.getJson(null, ethereum, euro), string.Empty);
+            Assert.AreEqual(pTest.getJson("ticker", bitcoin, fake), string.Empty);
+            Assert.AreNotEqual(pTest.getJson("ticker", bitcoin, ethereum), string.Empty);
+        }
+    }
+}
